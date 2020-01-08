208c15808af4d1fd00664e3d0750b69708179385
Vinicius Leonardo Okamoto Gonçalves <vinicius.goncalves@7comm.com.br>
2020-01-08 14:14:28 -0300

    DotNet, C# Learning

---
+++ cursodotnet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using cursodotnet.pilhas;
+
+namespace cursodotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        var s = new Pilha();
+        s.Empilha(1);
+        s.Empilha(10);
+        s.Empilha(100);
+        Console.WriteLine(s.Desempilha());
+        Console.WriteLine(s.Desempilha());
+        Console.WriteLine(s.Desempilha());
+
+        
+        }
+
+    }
+}
