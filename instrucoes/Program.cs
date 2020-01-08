7fcee26e3b2f1f5defaf49ea1b99fffa245df4d7
Vinicius Leonardo Okamoto Gonçalves <vinicius.goncalves@7comm.com.br>
2020-01-08 17:03:30 -0300

    instruções auxiliares

--- instrucoes/Program.cs
+++ instrucoes/Program.cs
@@ -4,9 +4,139 @@ namespace instrucoes
 {
     class Program
     {
+            static void Declaracoes()
+            {
+                int a;
+                int b = 2, c = 3;
+                const int d = 4;
+                a = 1;
+                Console.WriteLine(a + b + c + d);
+            }
+            static void InstucaoIf(string[] args){
+                if(args.Length == 0)
+                {
+                    Console.WriteLine(" Nenhum argumento");
+                }
+                else if(args.Length == 1)
+                {
+                    Console.WriteLine("Um argumento");
+                }
+                else
+                {
+                    Console.WriteLine($"{args.Length} argumentos");
+                }
+            }
+            static void InstrucaoSwitch(string[] args)
+            {
+                int numeroDeArgumentos = args.Length;
+                switch(numeroDeArgumentos)
+                {
+                    case 0:
+                        Console.WriteLine("Nenhum argumento");
+                        break;
+                    case 1:
+                        Console.WriteLine("Um argumento");
+                        break;
+                    default:
+                        Console.WriteLine("Nenhum argumento");
+                        break;
+                      
+                    
+                }     
+            }
+            static void IntrucaoWhile(string[] args)
+            {
+                int i = 0;
+                while (i< args.Length)
+                {
+                    Console.WriteLine($"Quantidade {args[i]}");
+                    i++;
+                }
+            }
+            static void IntrucaoDo(string[] args)
+            {
+                string texto;
+                do
+                {
+                    texto = Console.ReadLine();
+                    Console.WriteLine(texto);
+                } while (!string.IsNullOrEmpty(texto));
+            }
+            static void IntrucaoFor(string[] args)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+             static void IntrucaoForEach(string[] args)
+            {
+                foreach (string s in args)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+              static void IntrucaoBreak(string[] args)
+            {
+                while(true)
+                {
+                    string s = Console.ReadLine();
+                    if(string.IsNullOrEmpty(s)) break;
+                    
+                    Console.WriteLine(s);
+                }
+                
+            }
+              static void IntrucaoContinue(string[] args)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if(args[i].StartsWith("/")) continue;
+                    
+                    Console.WriteLine(args[i]);
+                }                
+            }
+              static void IntrucaoReturn(string[] args)
+            {
+                int Somar(int a, int b)
+                {
+                    return a + b;    
+                }            
+
+                if(args.Length == 0){
+                    Console.WriteLine("Não há argumento");
+                    return;
+                }
+                Console.WriteLine(Somar(4,5));
+                return;
+            }
+
+            static void IntrucoesTryCatchFinnalyThrow(string[] args)
+            {
+                double Dividir(double x, double y)
+                {
+                        if(y == 0 )
+                        throw new DivideByZeroException();
+                    return x / y;
+                }
+
+                try
+                {
+                    if(args.Length != 2)
+                    {
+                        throw new InvalidOperationException("Informe 2 números");
+                    }
+
+                    double x = double.Parse(args[0]);
+                    double y = double.Parse(args[1]);
+                    Console.WriteLine(Dividir(x, y));
+                }catch(InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
